52d463a9f36173e01066445ad45414e4cd70cc9e
Kayl Eubanks and Matt Dowdney <kayl+matt@>
2018-04-02 11:57:05 -0700

    add js logic to remove li elements

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@ function Item (title, details, priority) {
   this.title = title;
   this.details = details;
   this.priority = priority;
+  this.id = title.replace(/" "/g, "");
 }
+
 
 $(document).ready(function(){
   $("#listSubmit").submit(function(event){
@@ -12,6 +14,16 @@ $(document).ready(function(){
     var inputDetails = $("#details").val();
     var inputPriority = $("#priority").val();
 
+    var newItem = new Item(inputTitle, inputDetails, inputPriority);
+    console.log(newItem);
+
+    $("ul.highPriority").append("<li id='"+ newItem.id+"'>" + newItem.title + "<p class='"+newItem.id+"'>" + newItem.details + "</p><button class='button'>Completed!</button></li>");
+
+    $(".button").last().click(function() {
+      var listToRemove = document.getElementById(newItem.id);
+      console.log(listToRemove);
+      listToRemove.remove();
+    });
 
 
   });
