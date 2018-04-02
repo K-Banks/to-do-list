7681cf01c1d8d603da21ab4a1daee730264ce0a7
Kayl Eubanks and Matt Dowdney <kayl@>
2018-04-02 16:51:04 -0700

    adds new js logic

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,6 @@ Item.prototype.removeItem = function(target) {
   $("#priority").val("");
 }
 
-
 $(document).ready(function(){
   $("#listSubmit").submit(function(event){
     event.preventDefault();
@@ -22,19 +21,24 @@ $(document).ready(function(){
     var inputPriority = $("#priority").val();
     var newItem = new Item(inputTitle, inputDetails, inputPriority);
 
-
     if (newItem.priority[0] === "1"){
       $("ul.highPriority").append("<li>" + newItem.title + "<p>" + newItem.details + "</p><button class='button'>Completed!</button></li>");
+      $("ul.highPriority .button").last().click(function() {
+        newItem.removeItem(this);
+      });
     } else if (newItem.priority[0] === "2") {
       $("ul.lowPriority").append("<li>" + newItem.title + "<p>" + newItem.details + "</p><button class='button'>Completed!</button></li>");
+      $("ul.lowPriority .button").last().click(function() {
+        newItem.removeItem(this);
+      });
     } else {
       $("ul.mehPriority").append("<li>" + newItem.title + "<p>" + newItem.details + "</p><button class='button'>Completed!</button></li>");
+      $("ul.mehPriority .button").last().click(function() {
+        newItem.removeItem(this);
+      });
     }
     $("#title").val("");
     $("#details").val("");
     $("#priority").val("");
-    $(".button").last().click(function() {
-      newItem.removeItem(this);
-    });
   });
 });
