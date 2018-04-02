8f822ac9277728b8091b26e468891aa69290dc6e
Kayl Eubanks and Matt Dowdney <kayl@>
2018-04-02 15:45:53 -0700

    substantially clean js code and logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-var titleArray = [""];
-
 function Item (title, details, priority) {
   this.title = title;
   this.details = details;
@@ -7,15 +5,8 @@ function Item (title, details, priority) {
   this.id = title.replace(/" "/g, "");
 }
 
-Item.prototype.removeItem = function(newItem) {
-  var listToRemove = document.getElementById(newItem.id);
-  console.log(listToRemove);
-  listToRemove.remove();
-  for (var i = 0; i < titleArray.length; i++) {
-    if (titleArray[i] === newItem.title) {
-      titleArray.splice(i, 1);
-    }
-  }
+Item.prototype.removeItem = function(target) {
+  $(target).parents('li').remove();
   $("#title").val("");
   $("#details").val("");
   $("#priority").val("");
@@ -29,32 +20,21 @@ $(document).ready(function(){
     var inputTitle = $("#title").val();
     var inputDetails = $("#details").val();
     var inputPriority = $("#priority").val();
-    var titleCheck = 0;
+    var newItem = new Item(inputTitle, inputDetails, inputPriority);
 
-    for (var i = 0; i < titleArray.length; i++) {
-      if (inputTitle === titleArray[i]) {
-        titleCheck += 1;
-      }
-    }
 
-    if (titleCheck === 0) {
-      titleArray.push(inputTitle);
-      var newItem = new Item(inputTitle, inputDetails, inputPriority);
-      console.log(newItem);
-
-      if (newItem.priority[0] === "1"){
-        $("ul.highPriority").append("<li id='"+ newItem.id+"'>" + newItem.title + "<p id='"+newItem.id+"'>" + newItem.details + "</p><button class='button'>Completed!</button></li>");
-      } else if (newItem.priority[0] === "2") {
-        $("ul.lowPriority").append("<li id='"+ newItem.id+"'>" + newItem.title + "<p id='"+newItem.id+"'>" + newItem.details + "</p><button class='button'>Completed!</button></li>");
-      } else {
-        $("ul.mehPriority").append("<li id='"+ newItem.id+"'>" + newItem.title + "<p id='"+newItem.id+"'>" + newItem.details + "</p><button class='button'>Completed!</button></li>");
-      }
-      $(".button").last().click(function() {
-        newItem.removeItem(newItem);
-      });
+    if (newItem.priority[0] === "1"){
+      $("ul.highPriority").append("<li>" + newItem.title + "<p>" + newItem.details + "</p><button class='button'>Completed!</button></li>");
+    } else if (newItem.priority[0] === "2") {
+      $("ul.lowPriority").append("<li>" + newItem.title + "<p>" + newItem.details + "</p><button class='button'>Completed!</button></li>");
     } else {
-      alert("That task is already addded.")
+      $("ul.mehPriority").append("<li>" + newItem.title + "<p>" + newItem.details + "</p><button class='button'>Completed!</button></li>");
     }
-
+    $("#title").val("");
+    $("#details").val("");
+    $("#priority").val("");
+    $(".button").last().click(function() {
+      newItem.removeItem(this);
+    });
   });
 });
